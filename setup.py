8aacded49d941e681a9a9353be6a64df01b4bd68
Mattori <mattori.birnbaum@gmail.com>
2019-10-10 14:35:44 +0900

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+setup(name='powerline-wttrin',
+        version='1.0',
+        description='powerline segment for weather from wttr.in',
+        author='Mattori Birnbaum',
+        author_email='mattori.birnbaum@gmail.com',
+        url='https://github.com/PsychicNoodles/powerline-wttrin',
+        packages=find_packages(),
+        )
